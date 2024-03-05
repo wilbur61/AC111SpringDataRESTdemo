4842e92ce793a14a1eb3857041b09143b98377b5
wilbur61 <wroales@perscholas.org>
2024-03-05 14:57:02 -0500

    upgraded to Spring 3.2.2

---
+++ src/main/java/com/example/AC111SpringDataRESTdemo/Ac111SpringDataResTdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.AC111SpringDataRESTdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ac111SpringDataResTdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ac111SpringDataResTdemoApplication.class, args);
+	}
+
+}
